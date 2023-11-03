970c9ba6008c91ad9b0297396d019da46d210496
khrisn20 <khristian.narvaez@yahoo.com>
2023-11-03 17:04:24 -0400

    Refactored code

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-
-#Request headers
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"}
-
-
 #Get city, state_initials, and zip_code from user
 city = "tampa"
 state_initials = "fl"
 zip_code = "33602"
-
 first_search_url = f"https://www.autotrader.com/cars-for-sale/all-cars/{city}-{state_initials}?zip={zip_code}"
 
-#First Get request
-#response = request.get(first_search_url, headers=headers)
-#soup = bs(response.content, "html.parser")
+response = request.get(first_search_url, headers=headers)
+soup = bs(response.content, "html.parser")
 
-#Caching the response
-with open("response.html") as f:
-    soup = bs(f, 'html.parser')
-    
+search_results = soup.find("div", class_ = "padding-bottom-4 text-bold text-size-400 text-size-sm-500").text
+search_results = search_results.split()[0]
+first_record = 1
+number_of_records_per_page = 100
+url = f"https://www.autotrader.com/cars-for-sale/all-cars/{city}-{state_initials}?firstRecord={first_record}&isNewSearch=false&numRecords={number_of_records_per_page}&zip={zip_code}"
+
+#while first_record < (search_results // number_of_records_per_page):
     #Get Car Data (includes first)
     #cars_data = soup.find_all("h3")
     #for cd in cars_data:
@@ -36,11 +33,8 @@ with open("response.html") as f:
     #for cp in cars_price:
     #    print(cp.text)
     
-    #Get Search Results
-    #search_results = soup.find("div", class_ = "padding-bottom-4 text-bold text-size-400 text-size-sm-500").text
-    #print(search_results.split()[0])
+    #first_record += 100
 
-first_record = 1
-url = f"https://www.autotrader.com/cars-for-sale/all-cars/tampa-fl?firstRecord={first_record}&isNewSearch=false&numRecords=100&zip=33602"
+
 
 
