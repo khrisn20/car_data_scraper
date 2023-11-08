e79732d336a01c4d00addc1ead486785fcd53a2b
khrisn20 <khristian.narvaez@yahoo.com>
2023-11-08 10:57:33 -0500

    Debugging session

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"}
-#Get city, state_initials, and zip_code from user
 city = "tampa"
 state_initials = "fl"
 zip_code = "33602"
@@ -17,7 +16,7 @@ first_record = 1
 number_of_records_per_page = 100
 url = f"https://www.autotrader.com/cars-for-sale/all-cars/{city}-{state_initials}?firstRecord={first_record}&isNewSearch=false&numRecords={number_of_records_per_page}&zip={zip_code}"
 
-#while first_record < (search_results // number_of_records_per_page):
+#while first_record < search_results:
     #Get Car Data (includes first)
     #cars_data = soup.find_all("h3")
     #for cd in cars_data:
@@ -33,8 +32,7 @@ url = f"https://www.autotrader.com/cars-for-sale/all-cars/{city}-{state_initials
     #for cp in cars_price:
     #    print(cp.text)
     
-    #first_record += 100
-
+    #first_record += number_of_records_per_page
 
 
 
