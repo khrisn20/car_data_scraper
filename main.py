4bb75e3f49afc69bef71abcc3f05c239dd400d60
khrisn20 <khristian.narvaez@yahoo.com>
2023-11-09 09:46:19 -0500

    Created algo to scrape data

--- main.py
+++ main.py
@@ -7,32 +7,43 @@ state_initials = "fl"
 zip_code = "33602"
 first_search_url = f"https://www.autotrader.com/cars-for-sale/all-cars/{city}-{state_initials}?zip={zip_code}"
 
-response = request.get(first_search_url, headers=headers)
+response = requests.get(first_search_url, headers=headers)
 soup = bs(response.content, "html.parser")
 
 search_results = soup.find("div", class_ = "padding-bottom-4 text-bold text-size-400 text-size-sm-500").text
-search_results = search_results.split()[0]
+search_results = int(search_results.split()[0].replace(",", ""))
 first_record = 1
 number_of_records_per_page = 100
-url = f"https://www.autotrader.com/cars-for-sale/all-cars/{city}-{state_initials}?firstRecord={first_record}&isNewSearch=false&numRecords={number_of_records_per_page}&zip={zip_code}"
+url_base = f"https://www.autotrader.com/cars-for-sale/all-cars/{city}-{state_initials}?isNewSearch=false&zip={zip_code}"
 
-#while first_record < search_results:
-    #Get Car Data (includes first)
-    #cars_data = soup.find_all("h3")
-    #for cd in cars_data:
-    #    print(cd.text.split())
-
-    #Get Car Mileage (skips first)
-    #cars_mileage = soup.find_all("div", class_ = "item-card-specifications col-xs-9 margin-top-4 text-subdued-lighter")
-    #for cm in cars_mileage:
-    #    print(cm.text.split()[0])
-
-    #Get Car Price (includes first)
-    #cars_price = soup.find_all("span", class_ = "first-price text-ultra-bold")
-    #for cp in cars_price:
-    #    print(cp.text)
+cars_data = []
+while first_record < search_results:
+    print(first_record)
+    url = f"{url_base}&firstRecord={first_record}&numRecords={number_of_records_per_page}"
+    print(url)
+    response = requests.get(url, headers=headers)
+    print(response.status_code)
     
-    #first_record += number_of_records_per_page
+    soup = bs(response.content, "html.parser")
+    
+    cars_description = soup.find_all("h3")
+    del cars_description[0]
+
+    cars_price = soup.find_all("span", class_ = "first-price text-ultra-bold")
+    del cars_price[0]
+
+    cars_mileage = soup.find_all("div", class_ = "item-card-specifications col-xs-9 margin-top-4 text-subdued-lighter")
+
+    for i in range(len(cars_description)):
+        car = cars_description[i].text.split()
+        price = cars_price[i].text
+        mileage = cars_mileage[i].text.split()[0]
+    
+        car.append(price)
+        car.append(mileage)
+        cars_data.append(car)
+        
+    first_record += number_of_records_per_page
 
 
 
